5e641990d28208885dcbd67f5a5fdd3d9a460b60
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-11 10:25:21 +0100

    imported connect and created a variable to map state to props

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import SmurfForm from '../components/SmurfForm';
+import Axios from "axios";
+import {saveSmurfData} from '../state/actionCreator';
+import {connect} from 'react-redux';
+
 class App extends Component {
+
+  componentDidMount () {
+    Axios.get ('http://localhost:3333/smurfs')
+    .then((response) => {
+      console.log(response);
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -8,9 +20,15 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <SmurfForm/>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProp = state => {
+  return {
+
+  }
+}
+export default connect (mapStateToProp, {}) (App)
