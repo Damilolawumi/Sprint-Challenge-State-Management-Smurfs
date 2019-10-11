7ed8832bd7013a5c7620c0c3b8288b1468dfeecb
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-11 11:16:50 +0100

    created a method to post smurf data

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,17 +2,30 @@ import React, { Component } from "react";
 import "./App.css";
 import SmurfForm from '../components/SmurfForm';
 import Axios from "axios";
-import {saveSmurfData} from '../state/actionCreator';
-import {connect} from 'react-redux';
+import { saveSmurfData } from '../state/actionCreator';
+import { connect } from 'react-redux';
+import SmurfList from '../components/SmurfList';
 
 class App extends Component {
 
-  componentDidMount () {
-    Axios.get ('http://localhost:3333/smurfs')
-    .then((response) => {
-      console.log(response);
-    })
+  componentDidMount() {
+    Axios.get('http://localhost:3333/smurfs')
+      .then((response) => {
+        //dispatched an action to save response.data into the redux store
+        this.props.saveSmurfData(response.data);
+      })
   }
+
+  // created a method to post smurfdata 
+  saveSmurf = (data) => {
+    Axios.post('http://localhost:3333/smurfs', data)
+      .then((response) => {
+        //dispatched an action to save response.data into the redux store
+        this.props.saveSmurfData(response.data);
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -20,7 +33,8 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
-        <SmurfForm/>
+        <SmurfForm  saveSmurf={this.saveSmurf}/>
+        <SmurfList smurfs={this.props.smurfs} />
       </div>
     );
   }
@@ -28,7 +42,7 @@ class App extends Component {
 
 const mapStateToProp = state => {
   return {
-
+    smurfs: state.smurfs
   }
 }
-export default connect (mapStateToProp, {}) (App)
+export default connect(mapStateToProp, { saveSmurfData })(App)
