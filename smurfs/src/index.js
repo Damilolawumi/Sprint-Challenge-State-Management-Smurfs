f2ba83eaab769ef8042497a1c79b9cfca1d3a456
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-11 08:51:33 +0100

    imported dependencies to my index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import {Provider } from 'react-redux';
+import * as reducers from './state/reducers';
+import thunk from 'redux-thunk';
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
